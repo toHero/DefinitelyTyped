8ba9f69c9862e16847f0a2732e539ab598f66c25
Antoine Chabert <ancha@tohero.fr>
2016-08-10 14:34:22 +0200

    Introduce RRule definition

---
+++ rrule/rrule.d.ts
@@ -0,0 +1,22 @@
+﻿// Type definitions for RRule 2.1.0
+// Project: https://github.com/jkbrzt/rrule
+// Definitions by: Antoine Chabert <https://github.com/antitoine>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace RRule {
+
+    type Rule = {
+        cache?: boolean,
+        dtstart?: Date,
+        interval?: number,
+        wkst?: number | string,
+        count?: number,
+        until?: Date
+// TODO
+    }
+
+    export interface RRule {
+        RRule(rule: Rule);
+    }
+
+}
